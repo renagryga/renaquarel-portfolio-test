3a2118315027a6bb844bfc39244cf50ee3366cc4
renagryga <renagryga@gmail.com>
2026-07-26 21:58:00 +0200

    Update page.tsx

--- app/impressum/page.tsx
+++ app/impressum/page.tsx
@@ -2,7 +2,7 @@ export default function Impressum() {
   return (
     <main style={{display:"flex",justifyContent:"center",padding:"2rem"}}>
       <img
-        src="/images/impressum.png"
+        src="/renaquarel-portfolio-test/images/impressum.png"
         alt="Impressum"
         style={{maxWidth:"900px",width:"100%"}}
       />
