6a3d6507b7fa618195f0e90448f758e406de19fb
renagryga <renagryga@gmail.com>
2026-07-26 21:52:06 +0200

    Update page.tsx

--- app/datenschutz/page.tsx
+++ app/datenschutz/page.tsx
@@ -2,7 +2,7 @@ export default function Datenschutz() {
   return (
     <main style={{display:"flex",justifyContent:"center",padding:"2rem"}}>
       <img
-        src="/images/datenschutz.png"
+        src="/renaquarel-portfolio-test/images/datenschutz.png"
         alt="Datenschutzerklärung"
         style={{maxWidth:"900px",width:"100%"}}
       />
