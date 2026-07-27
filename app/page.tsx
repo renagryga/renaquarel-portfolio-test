f5ebfb2cfd5211693c185a2f0a09397b6e36da1b
renagryga <renagryga@gmail.com>
2026-07-27 13:26:56 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -14,11 +14,11 @@ export default function Home() {
         <picture>
           <source
             media="(max-width: 700px)"
-            srcSet={`${basePath}/images/hero/hero-mobile.jpg`}
+            srcSet={`${basePath}/images/hero/hero-mobile.jpg?v=3`}
           />
 
           <img
-            src={`${basePath}/images/hero/hero-desktop.jpg`}
+            src={`${basePath}/images/hero/hero-desktop.jpg?v=3`}
             alt="Portfolio von Rena Quarel – Illustratorin und Autorin"
             className="landingCover"
           />
