547da20f74f4d524e3d8597bfbd61c6c76a5a23a
renagryga <renagryga@gmail.com>
2026-07-27 13:18:55 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,22 @@
-import type { NextConfig } from "next";
-const repoName = process.env.GITHUB_REPOSITORY?.split("/")[1] ?? "";
-const isProjectPage = process.env.GITHUB_ACTIONS === "true" && repoName !== "renaquarel.github.io";
-const nextConfig: NextConfig = { output: "export", trailingSlash: true, images: { unoptimized: true }, basePath: isProjectPage ? `/${repoName}` : "", assetPrefix: isProjectPage ? `/${repoName}/` : "" };
+ import type { NextConfig } from "next";
+
+const repoName =
+  process.env.GITHUB_REPOSITORY?.split("/")[1] ?? "";
+
+const isProjectPage =
+  process.env.GITHUB_ACTIONS === "true" &&
+  repoName !== "renagryga.github.io";
+
+const nextConfig: NextConfig = {
+  output: "export",
+  trailingSlash: true,
+
+  images: {
+    unoptimized: true,
+  },
+
+  basePath: isProjectPage ? `/${repoName}` : "",
+  assetPrefix: isProjectPage ? `/${repoName}/` : "",
+};
+
 export default nextConfig;
