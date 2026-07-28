db63e7abd5c4c0233d2b127a4acfd26a9ce5a601
renagryga <renagryga@gmail.com>
2026-07-28 09:23:12 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,10 @@
+import type { Viewport } from "next";
+
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+};
+
 const basePath =
   process.env.NODE_ENV === "production"
     ? "/renaquarel-portfolio-test"
