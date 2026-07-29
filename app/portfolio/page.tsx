4e61b7adcb335a77a503919e4c39239337f5e451
renagryga <renagryga@gmail.com>
2026-07-29 14:18:04 +0200

    Update page.tsx

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -4,32 +4,32 @@ const basePath =
     : "";
 
 const images = [
-  "/images/portfolio/01.jpg",
-  "/images/portfolio/02.jpg",
-  "/images/portfolio/03.jpg",
-  "/images/portfolio/04.jpg",
-  "/images/portfolio/05.jpg",
-  "/images/portfolio/06.jpg",
-  "/images/portfolio/07.jpg",
-  "/images/portfolio/08.jpg",
-  "/images/portfolio/09.jpg",
-  "/images/portfolio/10.jpg",
-  "/images/portfolio/11.jpg",
-  "/images/portfolio/12.jpg",
-  "/images/portfolio/13.jpg",
+  "/images/portfolio/01.jpg.png",
+  "/images/portfolio/02.jpg.png",
+  "/images/portfolio/03.jpg.png",
+  "/images/portfolio/04.jpg.png",
+  "/images/portfolio/05.jpg.png",
+  "/images/portfolio/06.jpg.png",
+  "/images/portfolio/07.jpg.png",
+  "/images/portfolio/08.jpg.png",
+  "/images/portfolio/09.jpg.png",
+  "/images/portfolio/10.jpg.png",
+  "/images/portfolio/11.jpg.png",
+  "/images/portfolio/12.jpg.png",
+  "/images/portfolio/13.jpg.png",
 ];
 
 export default function PortfolioPage() {
   return (
     <main className="portfolioPage">
-      <nav className="siteNav" aria-label="Hauptnavigation">
+      <nav className="landingNav" aria-label="Hauptnavigation">
         <a href={`${basePath}/`}>Startseite</a>
         <a href={`${basePath}/portfolio/`}>Portfolio</a>
         <a href={`${basePath}/kontakt/`}>Kontakt</a>
         <a
           href="https://renaquarel.etsy.com"
           target="_blank"
-          rel="noreferrer"
+          rel="noopener noreferrer"
         >
           Shop
         </a>
